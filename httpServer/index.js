05b52f78ba288673ff5fdd7c9ded49d165c73d9a
ganesh2425 <saahoganesh023@gmail.com>
2021-04-06 21:12:44 +0530

    ok

--- httpServer/index.js
+++ httpServer/index.js
@@ -2,4 +2,6 @@ const http = require ("http");
 const server = http.createServer((response, request)=>{
     response.end("<h1>Hello Bangalore Good Morning to all</h1>");
 });
-server.listen(8000,"127.0.0.1",()=>{});
+server.listen(8000,"127.0.0.1",()=>{
+    console.log("new server is created by port no 8000");
+});
